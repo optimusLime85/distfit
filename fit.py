91b11ae9486a78325cb110a162a95dc247d81bfc
Mark Neuert <mark.neuert@gmail.com>
2018-06-17 11:22:13 -0600

    Formatted graphs, implemented Histogram.

--- fit.py
+++ fit.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import scipy.optimize as optimize
-import matplotlib.pyplot as plt
-from bokeh.io import output_file, show, curdoc
-from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Select
+from bokeh.io import curdoc
+from bokeh.layouts import column, row, widgetbox, gridplot
+from bokeh.models import ColumnDataSource, Select, Title
 from bokeh.plotting import figure
 
 
@@ -55,15 +54,18 @@ def callback(attr, old, new):
     _, dist_mle, dist_ls = calculate_fitted_data(df, dist_type)
 
     data_source.data['x_mle'] = dist_mle.ppf(data_source.data['cdf_y'])
+    data_source.data['pdf_mle'] = dist_mle.pdf(data_source.data['x_mle'])
     data_source.data['perc_mle'] = dist_mle.cdf(data_source.data['data'])
     data_source.data['quant_mle'] = dist_mle.ppf(data_source.data['perc_emp'])
 
     data_source.data['x_ls'] = dist_ls.ppf(data_source.data['cdf_y'])
+    data_source.data['pdf_ls'] = dist_ls.pdf(data_source.data['x_ls'])
     data_source.data['perc_ls'] = dist_ls.cdf(data_source.data['data'])
     data_source.data['quant_ls'] = dist_ls.ppf(data_source.data['perc_emp'])
 
     quantile_unity.data['x'] = (0, max(max(data_source.data['quant_ls']), max(data_source.data['quant_mle'])))
     quantile_unity.data['y'] = (0, max(max(data_source.data['quant_ls']), max(data_source.data['quant_mle'])))
+    # quantile_unity.data['y'] = (0, max(max(data_source.data['quant_ls']), max(data_source.data['quant_mle'])))
 
 # %% Get raw data.
 df = pd.read_csv('data.csv').dropna()
@@ -87,19 +89,10 @@ df['pdf_mle'] = dist_mle.pdf(df['x_mle'])
 df['x_ls'] = dist_ls.ppf(df['cdf_y'])
 df['pdf_ls'] = dist_ls.pdf(df['x_ls'])
 
-# db.plot(x='x_mle', y='pdf_mle')
-# db.plot(x='x_ls', y='pdf_ls', ax=plt.gca())
-# plt.hist(db['data'], density=True)
-#
-# db.plot(x='x_mle', y='cdf_mle')
-# db.plot(x='x_ls', y='cdf_ls', ax=plt.gca())
-# plt.scatter(x=db['data'], y=db['perc_emp'])
-#
-# plt.show()
-
 data_source = ColumnDataSource(df)
 quantile_unity = ColumnDataSource(dict(x=(0, max(max(df['quant_ls']), max(df['quant_mle']))),
                                        y=(0, max(max(df['quant_ls']), max(df['quant_mle'])))))
+# quantile_unity = ColumnDataSource(dict(x=(0, max(df['data'])), y=(0, max(df['data']))))
 
 # %% Calculate datapoints to represent the assumed distribution.
 demo_range = np.linspace(0.000001, 0.999999, 1000)
@@ -109,25 +102,52 @@ cdf_source = ColumnDataSource(data={
     'y': demo_range
 })
 
+# Define histogram
+bin_heights, bin_edges = np.histogram(df['data'], normed=True, bins='auto')
+hist_df = pd.DataFrame({'bin_heights': bin_heights})
+hist_df['bin_mids'] = pd.Series(bin_edges).rolling(window=2).mean().dropna().reset_index(drop=True)
+hist_df['bin_widths'] = pd.Series(bin_edges).diff().dropna().reset_index(drop=True)
+hist_source = ColumnDataSource(hist_df)
+
 # %% Define Bokeh plots
-cdf = figure(plot_width=400, tools='pan,box_zoom,reset')
+bin_range = max(bin_edges) - min(bin_edges)
+hist = figure(plot_width=400, tools='pan,box_zoom,reset', title='Histogram',
+              x_range=[min(bin_edges) - 0.1 * bin_range, max(bin_edges) + 0.1 * bin_range],
+              y_range=[0, max(bin_heights) * 1.1])
+hist.yaxis.axis_label = 'Probability Density'
+hist.yaxis.axis_label_text_font_style = 'bold'
+hist.vbar(x='bin_mids', width='bin_widths', top='bin_heights', source=hist_source, color='red')
+hist.line(x='x_mle', y='pdf_mle', color='green', source=data_source, line_width=3)
+hist.line(x='x_ls', y='pdf_ls', color='blue', source=data_source, line_width=3)
+
+cdf = figure(plot_width=400, tools='pan,box_zoom,reset', title='CDF')
 cdf.circle('data', 'perc_emp', color='gray', source=data_source, alpha=0.5)
 cdf.line('x_mle', 'cdf_y', color='green', source=data_source, line_width=3)
 cdf.line('x_ls', 'cdf_y', color='blue', source=data_source, line_width=3)
 
-pp = figure(plot_width=400, tools='pan,box_zoom,reset')
+pp = figure(plot_width=400, tools='pan,box_zoom,reset', title='pp')
+pp.xaxis.axis_label = 'Fitted Probabilities'
+pp.yaxis.axis_label = 'Empirical Probabilities'
+pp.xaxis.axis_label_text_font_style = 'bold'
+pp.yaxis.axis_label_text_font_style = 'bold'
 pp.circle('perc_mle', 'perc_emp', color='green', source=data_source)
 pp.circle('perc_ls', 'perc_emp', color='blue', source=data_source)
 pp.line(x=[0, 1], y=[0, 1], color='gray')
 
-qq = figure(plot_width=400, tools='pan,box_zoom,reset')
-qq.circle('data', 'quant_mle', color='green', source=data_source)
-qq.circle('data', 'quant_ls', color='blue', source=data_source)
+qq = figure(plot_width=400, tools='pan,box_zoom,reset', title='qq')
+qq.xaxis.axis_label = 'Fitted Quantiles'
+qq.yaxis.axis_label = 'Empirical Quantiles'
+qq.xaxis.axis_label_text_font_style = 'bold'
+qq.yaxis.axis_label_text_font_style = 'bold'
+qq.circle('quant_mle', 'data', color='green', source=data_source)
+qq.circle('quant_ls', 'data', color='blue', source=data_source)
 qq.line('x', y='y', color='gray', source=quantile_unity)
 
 options = [x for x in dir(stats) if isinstance(getattr(stats, x), stats.rv_continuous)]
 menu = Select(options=options, value='norm', title='Distribution')
+widgets = widgetbox(menu, width=400)
 menu.on_change('value', callback)
 
-layout = row(menu, cdf, column(pp, qq))
-curdoc().add_root(layout)
+grid = gridplot([hist, cdf, widgets],
+                [pp, qq, None])
+curdoc().add_root(grid)
