509bb0d7daca03798f7d54b4000efc519fd9ea38
Mark Neuert <m.neuert@cfertech.com>
2018-06-28 10:30:05 -0600

    Added fourplot function.

--- fit.py
+++ fit.py
@@ -94,3 +94,41 @@ def calc_fit_from_data(data, dist_type, loc='', alg='ls'):
     dist_ls = freeze_dist(dist_type, params_ls)
 
     return dist_ls
+
+
+def make_fourplot(data, dist, title='Title goes here', fig_save_path=None):
+    data = np.array(np.sort(data))
+    perc_emp = perc_emp_filliben(np.linspace(1, len(data), len(data)))
+    x_fit = dist.ppf(np.linspace(1e-6, (1 - 1e-6), 500))
+
+    fig, ((hist, cdf), (pp, qq)) = plt.subplots(2,2)
+    fig.suptitle(title)
+
+    hist.plot(x_fit, dist.pdf(x_fit), color='green', linewidth=2.)
+    n_bins = max(int(np.rint(np.sqrt(len(data)))), 10)
+    bins = np.linspace(min(data), max(data), n_bins + 1)
+    unrep_heights, _, _ = hist.hist(data, color='gray', bins=bins, density=True, label='Data', alpha=0.7)
+    hist.set_ylim([0, 1.1 * unrep_heights.max()])
+    hist.set_xlabel('Data')
+    hist.set_ylabel('Probability Density')
+
+    cdf.scatter(data, perc_emp, color='gray', s=1., alpha=0.7)
+    cdf.plot(x_fit, dist.cdf(x_fit), color='green', linewidth=2.)
+    cdf.set_xlabel('Data')
+    cdf.set_ylabel('CDF')
+
+    pp.scatter(dist.cdf(data), perc_emp, color='gray', s=1., alpha=0.7)
+    pp.plot((0, 1), (0,1), color='black', linewidth=1.)
+    pp.set_xlabel('Theoretical Probability')
+    pp.set_ylabel('Theoretical Probability')
+
+    qq.scatter(dist.ppf(perc_emp), data, color='gray', s=1., alpha=0.7)
+    qq_max = max(max(dist.ppf(perc_emp)), max(data))
+    qq.plot((0,qq_max), (0,qq_max), color='black', linewidth=1.)
+    qq.set_xlabel('Theoretical Quantile')
+    qq.set_ylabel('Empirical Quantile')
+
+    plt.tight_layout(rect=[0,0,1,0.95])
+
+    if fig_save_path:
+        plt.savefig(fig_save_path + '\\' + title + '.png')
