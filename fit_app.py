5586a6a02c34dd149548f82b4a8374d129a6adf3
optimusLime85 <mark.neuert@gmail.com>
2019-06-16 16:23:38 -0600

    Removed "if name=" statement so file runs every time.

--- fit_app.py
+++ fit_app.py
@@ -127,101 +127,101 @@ def on_dist_change(attr, old, new):
                                                 dist_mle, dist_ls, loc_val_input.value)
 
 
-if ('bk_script' in __name__) or (__name__ == '__main__'):
-    default_data_file = 'data.csv'
-    default_dist_type = 'norm'
-    data, dist_mle, dist_ls = load_data(default_data_file, default_dist_type, '')
+#if ('bk_script' in __name__) or (__name__ == '__main__'):
+default_data_file = 'data.csv'
+default_dist_type = 'norm'
+data, dist_mle, dist_ls = load_data(default_data_file, default_dist_type, '')
 
-    # Updated data_source.
-    data_source = ColumnDataSource(update_data_source(data, dist_mle, dist_ls))
+# Updated data_source.
+data_source = ColumnDataSource(update_data_source(data, dist_mle, dist_ls))
 
-    # Populate data_fit ColumnDataSource.
-    cdf_y = np.linspace(0.000001, 0.999999, 100)
-    data_fit_source = ColumnDataSource(update_data_fit_source(cdf_y, dist_mle, dist_ls))
+# Populate data_fit ColumnDataSource.
+cdf_y = np.linspace(0.000001, 0.999999, 100)
+data_fit_source = ColumnDataSource(update_data_fit_source(cdf_y, dist_mle, dist_ls))
 
-    # Populate metrics_source ColumnDataSource.
-    metrics_source = ColumnDataSource(update_metrics_source(('Source Data',
-                                                             'Maximum Likelihood',
-                                                             'Least Squares (Quantiles)'),
-                                                            data_source, dist_mle, dist_ls, ''))
+# Populate metrics_source ColumnDataSource.
+metrics_source = ColumnDataSource(update_metrics_source(('Source Data',
+                                                         'Maximum Likelihood',
+                                                         'Least Squares (Quantiles)'),
+                                                        data_source, dist_mle, dist_ls, ''))
 
-    # Define tools for Bokeh plots.
-    tools = 'pan,box_zoom,reset,save'
+# Define tools for Bokeh plots.
+tools = 'pan,box_zoom,reset,save'
 
-    # Histogram
-    bin_heights, bin_edges = np.histogram(data, normed=True, bins='auto')
-    hist_df = pd.DataFrame({'bin_heights': bin_heights})
-    hist_df['bin_mids'] = pd.Series(bin_edges).rolling(window=2).mean().dropna().reset_index(drop=True)
-    hist_df['bin_widths'] = pd.Series(bin_edges).diff().dropna().reset_index(drop=True)
-    hist_source = ColumnDataSource(hist_df)
-    bin_range = max(bin_edges) - min(bin_edges)
-    hist = figure(plot_width=400, plot_height=300, tools=tools, title='Histogram',
-                  x_range=[min(bin_edges) - 0.1 * bin_range, max(bin_edges) + 0.1 * bin_range],
-                  y_range=[0, max(bin_heights) * 1.1])
-    hist.yaxis.axis_label = 'Probability Density'
-    hist.yaxis.axis_label_text_font_style = 'bold'
-    hist.vbar(x='bin_mids', width='bin_widths', top='bin_heights', source=hist_source, color='red', legend='Data')
-    hist.line(x='x_mle', y='pdf_mle', color='green', source=data_fit_source, line_width=3, legend='MLE')
-    hist.line(x='x_ls', y='pdf_ls', color='blue', source=data_fit_source, line_width=3, legend='LS')
+# Histogram
+bin_heights, bin_edges = np.histogram(data, normed=True, bins='auto')
+hist_df = pd.DataFrame({'bin_heights': bin_heights})
+hist_df['bin_mids'] = pd.Series(bin_edges).rolling(window=2).mean().dropna().reset_index(drop=True)
+hist_df['bin_widths'] = pd.Series(bin_edges).diff().dropna().reset_index(drop=True)
+hist_source = ColumnDataSource(hist_df)
+bin_range = max(bin_edges) - min(bin_edges)
+hist = figure(plot_width=400, plot_height=300, tools=tools, title='Histogram',
+              x_range=[min(bin_edges) - 0.1 * bin_range, max(bin_edges) + 0.1 * bin_range],
+              y_range=[0, max(bin_heights) * 1.1])
+hist.yaxis.axis_label = 'Probability Density'
+hist.yaxis.axis_label_text_font_style = 'bold'
+hist.vbar(x='bin_mids', width='bin_widths', top='bin_heights', source=hist_source, color='red', legend='Data')
+hist.line(x='x_mle', y='pdf_mle', color='green', source=data_fit_source, line_width=3, legend='MLE')
+hist.line(x='x_ls', y='pdf_ls', color='blue', source=data_fit_source, line_width=3, legend='LS')
 
-    cdf = figure(plot_width=400, plot_height=300, tools=tools, title='CDF',
-                 x_range=[min(bin_edges) - 0.1 * bin_range, max(bin_edges) + 0.1 * bin_range])
-    cdf.circle('data', 'perc_emp', color='gray', source=data_source, alpha=0.5)
-    cdf.line('x_mle', 'cdf_y', color='green', source=data_fit_source, line_width=3)
-    cdf.line('x_ls', 'cdf_y', color='blue', source=data_fit_source, line_width=3)
+cdf = figure(plot_width=400, plot_height=300, tools=tools, title='CDF',
+             x_range=[min(bin_edges) - 0.1 * bin_range, max(bin_edges) + 0.1 * bin_range])
+cdf.circle('data', 'perc_emp', color='gray', source=data_source, alpha=0.5)
+cdf.line('x_mle', 'cdf_y', color='green', source=data_fit_source, line_width=3)
+cdf.line('x_ls', 'cdf_y', color='blue', source=data_fit_source, line_width=3)
 
-    # Probability plot
-    pp = figure(plot_width=400, plot_height=300, tools=tools, title='pp')
-    pp.xaxis.axis_label = 'Theoretical Probabilities'
-    pp.yaxis.axis_label = 'Empirical Probabilities'
-    pp.xaxis.axis_label_text_font_style = 'bold'
-    pp.yaxis.axis_label_text_font_style = 'bold'
-    pp.circle('perc_mle', 'perc_emp', color='green', source=data_source)
-    pp.circle('perc_ls', 'perc_emp', color='blue', source=data_source)
-    pp.line(x=[0, 1], y=[0, 1], color='gray')
+# Probability plot
+pp = figure(plot_width=400, plot_height=300, tools=tools, title='pp')
+pp.xaxis.axis_label = 'Theoretical Probabilities'
+pp.yaxis.axis_label = 'Empirical Probabilities'
+pp.xaxis.axis_label_text_font_style = 'bold'
+pp.yaxis.axis_label_text_font_style = 'bold'
+pp.circle('perc_mle', 'perc_emp', color='green', source=data_source)
+pp.circle('perc_ls', 'perc_emp', color='blue', source=data_source)
+pp.line(x=[0, 1], y=[0, 1], color='gray')
 
-    # Quantile plot
-    qq = figure(plot_width=400, plot_height=300, tools=tools, title='qq')
-    qq.xaxis.axis_label = 'Theoretical Quantiles'
-    qq.yaxis.axis_label = 'Empirical Quantiles'
-    qq.xaxis.axis_label_text_font_style = 'bold'
-    qq.yaxis.axis_label_text_font_style = 'bold'
-    qq.circle('quant_mle', 'data', color='green', source=data_source)
-    qq.circle('quant_ls', 'data', color='blue', source=data_source)
-    qq_line_source = ColumnDataSource(dict(x=(0, max(data)), y=(0, max(data))))
-    qq.line(x='x', y='y', color='gray', source=qq_line_source)
+# Quantile plot
+qq = figure(plot_width=400, plot_height=300, tools=tools, title='qq')
+qq.xaxis.axis_label = 'Theoretical Quantiles'
+qq.yaxis.axis_label = 'Empirical Quantiles'
+qq.xaxis.axis_label_text_font_style = 'bold'
+qq.yaxis.axis_label_text_font_style = 'bold'
+qq.circle('quant_mle', 'data', color='green', source=data_source)
+qq.circle('quant_ls', 'data', color='blue', source=data_source)
+qq_line_source = ColumnDataSource(dict(x=(0, max(data)), y=(0, max(data))))
+qq.line(x='x', y='y', color='gray', source=qq_line_source)
 
-    # Distribution dropdown widget
-    options = [x for x in dir(stats) if isinstance(getattr(stats, x), stats.rv_continuous)]
-    dist_menu = Select(options=options, value='norm', title='Distribution:')
-    dist_menu.on_change('value', on_dist_change)
+# Distribution dropdown widget
+options = [x for x in dir(stats) if isinstance(getattr(stats, x), stats.rv_continuous)]
+dist_menu = Select(options=options, value='norm', title='Distribution:')
+dist_menu.on_change('value', on_dist_change)
 
-    # Data source dropdown widget
-    files = [x for x in os.listdir('data') if x.split('.')[-1] == 'csv']
-    data_source_menu = Select(options=files, value='data.csv', title='Source from \'data\' directory:')
-    data_source_menu.on_change('value', on_change_data_source)
+# Data source dropdown widget
+files = [x for x in os.listdir('data') if x.split('.')[-1] == 'csv']
+data_source_menu = Select(options=files, value='data.csv', title='Source from \'data\' directory:')
+data_source_menu.on_change('value', on_change_data_source)
 
-    # Table widget
-    num_format = NumberFormatter()
-    num_format.format = '0.00000'
-    metrics_columns = [
-        TableColumn(field='method', title='Source'),
-        TableColumn(field='mean', title='Mean', formatter=num_format),
-        TableColumn(field='sd', title='Std Dev', formatter=num_format),
-        TableColumn(field='loc', title='Loc Param', formatter=num_format),
-        TableColumn(field='scale', title='Scale Param', formatter=num_format),
-        TableColumn(field='shape', title='Shape Param', formatter=num_format),
-        TableColumn(field='aic', title='AIC', formatter=num_format),
-    ]
-    metrics_table = DataTable(source=metrics_source, columns=metrics_columns, height=100)
+# Table widget
+num_format = NumberFormatter()
+num_format.format = '0.00000'
+metrics_columns = [
+    TableColumn(field='method', title='Source'),
+    TableColumn(field='mean', title='Mean', formatter=num_format),
+    TableColumn(field='sd', title='Std Dev', formatter=num_format),
+    TableColumn(field='loc', title='Loc Param', formatter=num_format),
+    TableColumn(field='scale', title='Scale Param', formatter=num_format),
+    TableColumn(field='shape', title='Shape Param', formatter=num_format),
+    TableColumn(field='aic', title='AIC', formatter=num_format),
+]
+metrics_table = DataTable(source=metrics_source, columns=metrics_columns, height=100)
 
-    # Text input widget
-    loc_val_input = TextInput(title='Specify loc value:', placeholder='none', value='')
-    loc_val_input.on_change('value', on_dist_change)
+# Text input widget
+loc_val_input = TextInput(title='Specify loc value:', placeholder='none', value='')
+loc_val_input.on_change('value', on_dist_change)
 
-    # Format app layout
-    widgets = widgetbox(metrics_table, dist_menu, data_source_menu, loc_val_input, width=400)
-    grid = gridplot([hist, cdf],
-                    [pp, qq],
-                    [widgets, None])
-    curdoc().add_root(grid)
+# Format app layout
+widgets = widgetbox(metrics_table, dist_menu, data_source_menu, loc_val_input, width=400)
+grid = gridplot([hist, cdf],
+                [pp, qq],
+                [widgets, None])
+curdoc().add_root(grid)
